dded002b0ee68d7206bbccb9a2dccb9b10a66092
James Koval <james.ross.koval@gmail.com>
2013-09-19 01:12:23 -0700

    automate everything

--- install.sh
+++ install.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
-echo '
-Run these commands if you installed globally with -g
-  echo PATH=\$PATH:`npm -g list --parseable git-new-workdir | head -n1` >> ~/.bashrc
-  echo . `npm -g list --parseable git-bd | head -n1`/bd.bashrc >> ~/.bashrc
 
-Run these commands if you installed locally without -g
-  echo PATH=\$PATH:`npm list --parseable git-bd | head -n1` >> ~/.bashrc
-  echo PATH=\$PATH:`npm list --parseable git-new-workdir | head -n1` >> ~/.bashrc
-  echo . `npm list --parseable git-bd | head -n1`/bd.bashrc >> ~/.bashrc
+####  ~/.bashrc.d/ shall now exist - useful hack to load many .bashrc files
+mkdir -p ~/.bashrc.d/
+SOURCE_RC_FILES='for file in ~/.bashrc.d/*; do source $file; done'
+if ! fgrep -q "$SOURCE_RC_FILES" ~/.bashrc; then
+  echo "$SOURCE_RC_FILES" >> ~/.bashrc
+fi
+
+
+####  Delete previous installation
+INSTALL_PATH=~/.bashrc.d/git-bd.sh
+cat /dev/null > $INSTALL_PATH
+
+
+####  Install git-bd and the bd alias
+echo PATH=\$PATH:`pwd` >> $INSTALL_PATH
+echo source `pwd`/bd.bashrc >> $INSTALL_PATH
+
+
+####  Inform the user of installation method
+echo '
+Success: git-bd environment settings are now in ~/.bashrc.d/git-bd.sh
+Tip: ~/.bashrc.d/* are files loaded after your ~/.bashrc file.
+Please restart your shell now, or source ~/.bashrc
 '
